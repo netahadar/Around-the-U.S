1cfecb8ba8fe7fcd9354ba7953b60d50871afc73
Neta Hadar Itzhak <netook6@gmail.com>
2021-08-23 14:29:24 +0300

    fix according to first review

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let popupWindow = document.querySelector(".popup");
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".popup__close-button");
-let submitButton = document.querySelector(".popup__form-submit-button");
-let formNameInput = document.querySelector(".popup__form-input_name");
-let formJobInput = document.querySelector(".popup__form-input_job");
+let form = document.querySelector(".popup__form");
+let formNameInput = document.querySelector(".popup__form_input_name");
+let formJobInput = document.querySelector(".popup__form_input_job");
 let profileName = document.querySelector(".profile__name");
 let profileJobTitle = document.querySelector(".profile__job-description");
 
 function openPopup() {
     popupWindow.classList.add("popup_opened");
+    formNameInput.value = profileName.textContent;
+    formJobInput.value = profileJobTitle.textContent; 
 }
 
 function closePopup() {
@@ -19,11 +21,9 @@ function formSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = formNameInput.value;
     profileJobTitle.textContent = formJobInput.value;
+    closePopup();
   } 
 
 editButton.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopup);
-submitButton.addEventListener("click", formSubmit);
-
-formNameInput.value = profileName.textContent;
-formJobInput.value = profileJobTitle.textContent; 
+form.addEventListener("submit", formSubmit);
