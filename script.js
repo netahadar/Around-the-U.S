3b767132bed46f40bc463cdb37e9e9b20b0f0411
Neta Hadar Itzhak <netook6@gmail.com>
2021-08-26 00:50:46 +0300

    refactor: create initial gallery using JS

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-let popupWindow = document.querySelector(".popup");
-let editButton = document.querySelector(".profile__edit-button");
-let closeButton = document.querySelector(".popup__close-button");
-let form = document.querySelector(".popup__form");
-let formNameInput = document.querySelector(".popup__form-input_field_name");
-let formJobInput = document.querySelector(".popup__form-input_field_job");
-let profileName = document.querySelector(".profile__name");
-let profileJobTitle = document.querySelector(".profile__job-description");
+const popupWindow = document.querySelector(".popup");
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".popup__close-button");
+const form = document.querySelector(".popup__form");
+const formNameInput = document.querySelector(".popup__form-input_field_name");
+const formJobInput = document.querySelector(".popup__form-input_field_job");
+const profileName = document.querySelector(".profile__name");
+const profileJobTitle = document.querySelector(".profile__job-description");
+const galleryList = document.querySelector(".gallery__list");
+const galleryTemplae = document.querySelector("#gallery-post").content;
+const initialGalleryItems = [
+    {
+        name: "Huntington beach",
+        link: "./blocks/images/huntington-beach.jpg"
+    },
+    {
+        name: "Vermont",
+        link: "./blocks/images/vermont.jpg"
+    },
+    {
+        name: "Mount Rainier Washington",
+        link: "./blocks/images/mount-Rainier-washington.jpg"
+    },
+    {
+        name: "San Fransisco",
+        link: "./blocks/images/san-francisco.jpg"
+    },
+    {
+        name: "Sedona",
+        link: "./blocks/images/sedona.jpg"
+    },
+    {
+        name:"Montauk beach",
+        link: "./blocks/images/montauk.jpg"
+    },
+]
+
+function createGallery(myArray) {
+    initialGalleryItems.forEach( function (item) {
+        const galleryItem = galleryTemplae.querySelector(".gallery__item").cloneNode(true);
+        galleryItem.querySelector(".gallery__photo").src = item.link;
+        galleryItem.querySelector(".gallery__photo").alt = item.name;
+        galleryItem.querySelector(".gallery__text").textContent = item.name;
+        galleryList.append(galleryItem);
+    }
+        );    
+}
 
 function openPopup() {
     popupWindow.classList.add("popup_opened");
@@ -24,6 +63,11 @@ function formSubmit(evt) {
     closePopup();
   } 
 
+
+
+
+
+createGallery(initialGalleryItems);
 editButton.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopup);
 form.addEventListener("submit", formSubmit);
