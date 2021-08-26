efc398b7a3a4bfa58ce26bfb9bbcd10944930578
Neta Hadar Itzhak <netook6@gmail.com>
2021-08-26 16:06:01 +0300

    refactor: create popups for display images and create post, feat: add delete button, like button and add button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Profile edit popup's objects:
 const popupWindow = document.querySelector(".popup");
 const editButton = document.querySelector(".profile__edit-button");
 const profileEditCloseButton = document.querySelector(".popup__close-button");
@@ -6,6 +7,14 @@ const formNameInput = document.querySelector(".popup__form-input_field_name");
 const formJobInput = document.querySelector(".popup__form-input_field_job");
 const profileName = document.querySelector(".profile__name");
 const profileJobTitle = document.querySelector(".profile__job-description");
+
+// Full sized photo popup's objects:
+const photoPopup = document.querySelector(".popup_type_photo");
+const fullScreenPhoto = document.querySelector(".popup__photo");
+const popupPhotoDescription = document.querySelector(".popup__text");
+const closePhotoPopup = document.querySelector(".popup__close-button_type_photo");
+
+// New post popup's objects:
 const addPostButton = document.querySelector(".profile__add-button");
 const newPostPopup = document.querySelector(".popup_type_post");
 const postCloseButton = document.querySelector(".popup__close-button_type_post");
@@ -13,7 +22,7 @@ const createPostForm = document.querySelector(".popup__form_type_new-post");
 const postTitle = document.querySelector(".popup__form-input_field_title");
 const postLink = document.querySelector(".popup__form-input_field_link");
 const galleryList = document.querySelector(".gallery__list");
-const galleryTemplae = document.querySelector("#gallery-post").content;
+const galleryTemplate = document.querySelector("#gallery-post").content;
 const initialGalleryItems = [
     {
         name: "Huntington beach",
@@ -41,62 +50,98 @@ const initialGalleryItems = [
     },
 ]
 
-function createGallery(myArray) {
-    myArray.forEach(function (item) {
-        const galleryItem = galleryTemplae.querySelector(".gallery__item").cloneNode(true);
-        galleryItem.querySelector(".gallery__photo").src = item.link;
-        galleryItem.querySelector(".gallery__photo").alt = item.name;
-        galleryItem.querySelector(".gallery__text").textContent = item.name;
-        galleryList.append(galleryItem);
-    }
-    );
+
+//Create new gallery Item:
+
+function createGalleryPost(name, link) {
+    // Create post:
+    const galleryItem = galleryTemplate.querySelector(".gallery__item").cloneNode(true);
+    galleryItem.querySelector(".gallery__photo").src = link;
+    galleryItem.querySelector(".gallery__photo").alt = name;
+    galleryItem.querySelector(".gallery__text").textContent = name;
+
+    // Event listener to trash button:
+    const deleteButton = galleryItem.querySelector(".gallery__trash-button");
+    deleteButton.addEventListener("click", function (evt) {
+        const parentItem = evt.target.closest(".gallery__item");
+        parentItem.remove();
+    });
+
+    //Event listener to like button:
+    const likeButton = galleryItem.querySelector(".gallery__like-button");
+    likeButton.addEventListener("click", function (evt) {
+        const button = evt.target;
+        button.classList.toggle("gallery__like-button_active");
+    });
+
+    //Event listener to photo popup:
+    const galleryPhoto = galleryItem.querySelector(".gallery__photo");
+    galleryPhoto.addEventListener("click", function () {
+        photoPopup.classList.add("popup_opened");
+        fullScreenPhoto.setAttribute("src", link);
+        fullScreenPhoto.setAttribute("alt", name);
+        popupPhotoDescription.textContent = name;
+    });
+    
+    // Close new post's popup when submitting:
+    newPostPopup.classList.remove("popup_opened");
+
+    return galleryItem;
 }
 
+// Open profile edit's popup:
 function openPopup() {
     popupWindow.classList.add("popup_opened");
     formNameInput.value = profileName.textContent;
     formJobInput.value = profileJobTitle.textContent;
 }
 
+//Close profile edit's popup:
 function closePopup() {
     popupWindow.classList.remove("popup_opened");
 }
 
-function formSubmit(evt) {
+// Submit profile edit's form:
+function submitForm(evt) {
     evt.preventDefault();
     profileName.textContent = formNameInput.value;
     profileJobTitle.textContent = formJobInput.value;
     closePopup();
 }
 
-function postSubmit(evt) {
+// Submit new post's form:
+function submitPost(evt) {
     evt.preventDefault();
-    const newArray = [];
 
+    // content validation:
     if (postTitle.valeue == "" || postLink.value == "") {
         alert("please insert image's title and link, or press the close button");
     } else {
-        const postDetails = {
-            name: postTitle.value,
-            link: postLink.value
-        }
-
-        newArray.push(postDetails);
-        createGallery(newArray);
-        newPostPopup.classList.remove("popup_opened");
+        name = postTitle.value;
+        link = postLink.value;
+        galleryList.prepend(createGalleryPost(name, link));
     }
 }
 
 
+// Create first 6 posts:
+initialGalleryItems.forEach(function (item) {
+    return galleryList.append(createGalleryPost(item.name, item.link));
+});
 
-createGallery(initialGalleryItems);
+//Event listeners for profile edit:
 editButton.addEventListener("click", openPopup);
 profileEditCloseButton.addEventListener("click", closePopup);
+form.addEventListener("submit", submitForm);
+
+//Event listeners for adding a new post:
 addPostButton.addEventListener("click", function () {
     newPostPopup.classList.add("popup_opened");
 });
 postCloseButton.addEventListener("click", function () {
     newPostPopup.classList.remove("popup_opened");
 });
-form.addEventListener("submit", formSubmit);
-createPostForm.addEventListener("submit", postSubmit);
+createPostForm.addEventListener("submit", submitPost);
+closePhotoPopup.addEventListener("click", function () {
+    photoPopup.classList.remove("popup_opened");
+});
